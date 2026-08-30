64f71cb5eb6902480626c60513424de52f06dc03
Hor Xiang Zhi <horxiangzhi@gmail.com>
2026-08-30 15:10:29 +0800

    Q2.2

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,49 +1,48 @@
 
-
 public class SinglyLinkedList<E> {
-    
+
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
 
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -52,8 +51,8 @@ public class SinglyLinkedList<E> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -61,18 +60,18 @@ public class SinglyLinkedList<E> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
     }
 
     // Write your codes below
-    public String toString(){
+    public String toString() {
         Node<E> cur = head;
         StringBuilder res = new StringBuilder();
 
-        while(cur != null) {
+        while (cur != null) {
             res.append(cur.getElement()).append(" ");
             cur = cur.getNext();
         }
@@ -81,14 +80,14 @@ public class SinglyLinkedList<E> {
 
     }
 
-    public E removeLast(){
-        if(isEmpty()) {
+    public E removeLast() {
+        if (isEmpty()) {
             return null;
         }
 
         E answer = tail.getElement();
 
-        if(head == tail) {
+        if (head == tail) {
             head = null;
             tail = null;
             size--;
@@ -99,7 +98,7 @@ public class SinglyLinkedList<E> {
 
         // continue until we find the second last
         while (startPtr.getNext() != tail) {
-            startPtr = startPtr.getNext(); 
+            startPtr = startPtr.getNext();
         }
 
         // update tail to that and set the next to null and reduce the size
@@ -110,19 +109,18 @@ public class SinglyLinkedList<E> {
         return answer;
     }
 
-    public void reverse(){       
+    public void reverse() {
 
-        if(size == 1) {
+        if (size == 1) {
             return;
         }
-
 
         Node<E> prev = null;
         Node<E> cur = head;
 
         tail = head;
 
-        while (cur != null){
+        while (cur != null) {
             // store the next
             Node<E> next = cur.getNext();
 
@@ -131,7 +129,7 @@ public class SinglyLinkedList<E> {
 
             // set the prev to cur
             prev = cur;
-            
+
             // set cur to next
             cur = next;
         }
